13e97ae4a98614ef8566090465994cf05d18545b
BE-Webdesign <cromleyedwin89@gmail.com>
2016-08-22 11:01:35 -0400

    Change Route Testing File
    This changes the routing test file to have a successful test along with
    new namespacing uses.

--- tests/test-route.php
+++ tests/test-route.php
@@ -5,13 +5,18 @@
  * @package BEforever\Routing\Tests
  */
 
+namespace BEforever\Routing\Tests;
+use \PHPUnit\Framework as PHPUnit;
+use \BEforever\Routing\App as Routing;
 
 /**
  * Test Case for the route entity.
  */
+class RouteTest extends PHPUnit\TestCase {
 	/**
 	 * Test whether the Route class exists.
 	 */
 	public function test_route_exists() {
+		$this->assertTrue( class_exists( 'BEforever\Routing\App\Route' ) );
 	}
 }
