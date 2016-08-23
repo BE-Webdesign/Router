68a6387e5ecff196b437fde1209f7c74db68b812
BE-Webdesign <cromleyedwin89@gmail.com>
2016-08-23 13:12:27 -0400

    Initial Route Class
    This is the intial commit of the Route class's functionality.  It
    features one main function `dispatch()`, which fires the registered
    callback.  The resource is what should be matched by the router.
    Eventually this will become an interface that more specific routes can
    implement.

--- app/route.php
+++ app/route.php
@@ -14,7 +14,75 @@ namespace BEforever\Routing\App;
  */
 class Route {
 
-	public function __construct() {
-		
+	/**
+	 * The input for a route that is supposed to match a resource.
+	 *
+	 * @var mixed $resource
+	 */
+	private $resource;
+
+	/**
+	 * Callback registered to the route.
+	 *
+	 * The callback can be provided in $options['callback']. The callback is
+	 * fired in the dispatch() method.
+	 *
+	 * @var callable|string $callback
+	 */
+	private $callback;
+
+	/**
+	 * Each route should map to one resource and can support options.
+	 *
+	 * The $resource will typically be the path of a URI. $options should be
+	 * specified as an array of various configuration settings that are tied to
+	 * this particular route.
+	 *
+	 * @param mixed $resource Resource of the route.
+	 * @param array $options  Configuration options for the route.
+	 */
+	public function __construct( $resource, array $options ) {
+		$this->resource = $resource;
+
+		$this->parse_options( $options );
+	}
+
+	/**
+	 * Dispatches the routes callback.
+	 *
+	 * Note: The route is flexible in its use. You can map routes to output
+	 * callbacks or to fetchable callbacks.
+	 *
+	 * @throws \BadFunctionCallException If the callback is not valid.
+	 */
+	public function dispatch() {
+		if ( is_callable( $this->callback ) ) {
+			return call_user_func( $this->callback );
+		} else {
+			throw new \BadFunctionCallException( 'The route callback is not a valid callback.' );
+		}
+	}
+
+	/**
+	 * Returns the private variable $resource.
+	 */
+	public function get_resource() {
+		return $this->resource;
+	}
+
+	/**
+	 * Returns the private variable $callback.
+	 */
+	public function get_callback() {
+		return $this->callback;
+	}
+
+	/**
+	 * Parse the configuration options into the object.
+	 *
+	 * @param array $options Options for route configuration. Should contain callback.
+	 */
+	public function parse_options( array $options ) {
+		$this->callback = isset( $options['callback'] ) ? $options['callback'] : null;
 	}
 }
--- tests/test-route.php
+++ tests/test-route.php
@@ -13,9 +13,88 @@ use \BEforever\Routing\App as Routing;
  */
 class RouteTest extends PHPUnit_Framework_TestCase {
 	/**
+	 * Name of class that is being tested.
+	 */
+	private $class_name = 'BEforever\Routing\App\Route';
+
+	/**
 	 * Test whether the Route class exists.
 	 */
 	public function test_class_exists() {
-		$this->assertTrue( class_exists( 'BEforever\Routing\App\Route' ) );
+		$this->assertTrue( class_exists( $this->class_name ) );
+	}
+
+	/**
+	 * Test whether the various properties exist.
+	 */
+	public function test_class_properties_exist() {
+		$this->assertTrue( property_exists( $this->class_name, 'resource' ) );
+		$this->assertTrue( property_exists( $this->class_name, 'callback' ) );
+	}
+
+	/**
+	 * Test whether the various methods exist.
+	 */
+	public function test_class_methods_exist() {
+		$this->assertTrue( method_exists( $this->class_name, 'dispatch' ) );
+		$this->assertTrue( method_exists( $this->class_name, 'get_resource' ) );
+		$this->assertTrue( method_exists( $this->class_name, 'get_callback' ) );
+		$this->assertTrue( method_exists( $this->class_name, 'parse_options' ) );
+	}
+
+	/**
+	 * Verify that object constructor method works as intended.
+	 */
+	public function test_route_constructor() {
+		$route = new Routing\Route( '/example-route', array() );
+
+		$this->assertEquals( '/example-route', $route->get_resource() );
+	}
+
+	/**
+	 * Tests the parsing of route options.
+	 */
+	public function test_route_parse_options() {
+		$options = array(
+			'callback' => 'example_function',
+		);
+
+		$route = new Routing\Route( '/', $options );
+
+		$this->assertEquals( 'example_function', $route->get_callback() );
+	}
+
+	/**
+	 * Test a successful route dispatch.
+	 */
+	public function test_route_dispatch_success() {
+		$this->expectOutputString( 'Test Succeeded' );
+
+		$callback = array( $this, 'example_dispatch' );
+
+		$options = array(
+			'callback' => $callback,
+		);
+
+		$route = new Routing\Route( '/', $options );
+		$route->dispatch();
+	}
+
+	/**
+	 * Test route dispatch failure.
+	 */
+	public function test_route_dispatch_failure() {
+		$this->expectException( \BadFunctionCallException::class );
+
+		// No callback specified.
+		$route = new Routing\Route( '/', array() );
+		$route->dispatch();
+	}
+
+	/**
+	 * A helper function for testing route dispatching.
+	 */
+	public static function example_dispatch() {
+		echo 'Test Succeeded';
 	}
 }
